eb20c186a3abbbb713a715f0b5c21984f0f205a0
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2018-07-27 20:46:03 +0900

    関数諸々追加

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,30 @@
 import '../scss/index.scss'
+import 'babel-polyfill'
 
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 class HelloMessage extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return <div>Hello {this.props.name}</div>
   }
 }
+const url = 'http://api.myjson.com/bins/159wqn'
+const asyncFunc = async url => {
+  console.log(await fetch(url))
+}
 
-const obj1 = {value: 1}
-const obj2 = {value: 2}
-const obj3 = {test: 'heyheyhey'}
-const nera = {...obj1, ...obj2, ...obj3}
-console.log(nera)
+asyncFunc(url).then(() => console.log('done'))
 
-const nera1 = ({test}) => console.log(test)
+const obj1 = { value: 1 }
+const obj2 = { value: 2 }
+const obj3 = { test: 'hey' }
+const combinedObject = { ...obj1, ...obj2, ...obj3 }
+console.log(combinedObject)
 
-nera1(nera)
+const func1 = ({ test }) => console.log(test)
 
-const mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+func1(combinedObject)
+
+const mountNode = document.getElementById('app')
+ReactDOM.render(<HelloMessage name="Jane" />, mountNode)
